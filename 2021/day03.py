aca23c4ec779ea9d9893b7301f363e2d4836989b
P Handbauer <loserock@users.noreply.github.com>
2022-07-20 17:56:11 +0200

    implement day3 second task, bugfixes

--- 2021/day03.py
+++ 2021/day03.py
@@ -7,24 +7,47 @@ def get_data(s: str) -> np.ndarray:
     return np.array([list(map(int, l)) for l in s.split('\n')], dtype='b')
 
 
-def first(data: np.ndarray):
+def first(data: np.ndarray) -> int:
     N = len(data[:, 0])
     L = len(data[0])
-    gamma = int()  # gamma: most common
-    # epsilon: least common
     most_common_bits = ''.join(
         map(lambda i: str(int(i)), np.sum(data, axis=0) > N/2))
-    gamma = int(most_common_bits, 2)
-    epsilon = gamma ^ int('1'*L, 2)
+    gamma = int(most_common_bits, 2)  # gamma: most common
+    epsilon = gamma ^ int('1'*L, 2)  # epsilon: least common
     return gamma * epsilon
 
 
+def second(data: np.ndarray):
+    N = len(data[:, 0])
+    L = len(data[0])
+    most_common_by_pos = lambda d, pos: int(np.sum(d[:, pos]) >= len(d[:, 0]) / 2)
+    # select by most common bits
+    d = data.copy()
+    for p in range(L):
+        mc = most_common_by_pos(d, p)
+        d = np.array([n for n in d if n[p]==mc])
+        if len(d) == 1:
+            oxygen = int(''.join(map(str, d[0])), 2)
+            break
+    # select by least common bits
+    d = data.copy()
+    for p in range(L):
+        mc = most_common_by_pos(d, p)
+        d = np.array([n for n in d if n[p]!=mc])
+        if len(d) == 1:
+            co2 = int(''.join(map(str, d[0])), 2)
+            break
+    print(f"oxigen: {oxygen}, co2: {co2}")
+    return oxygen * co2
+
+
 def main():
-    # TODO get input data to `s`
+    global s  # TODO get input data to `s`
     d = get_data(s)
     r1 = first(d)
-    print(f"For the 5th: {r1}; ...")
+    r2 = second(d)
+    print(f"For the 5th: {r1}; and the 6th: {r2}")
 
 
-if __name__ == "__main___":
+if __name__ == "__main__":
     main()
